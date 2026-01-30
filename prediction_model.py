b9932fcf73ac2436d168dc1d8a701c9b7deb8f3d
Aleena <aleenaamir02@gmail.com>
2026-01-30 16:51:33 +0500

    Fix plotting and paths for GitHub Actions

--- prediction_model.py
+++ prediction_model.py
@@ -5,8 +5,8 @@ from pathlib import Path
 # =========================
 # 1. LOAD FEATURE DATA
 # =========================
-feature_file = Path("feature_engineered_historical_with_realtime.pkl")
-df = pd.read_pickle(feature_file)
+#feature_file = Path("feature_engineered_historical_with_realtime.pkl")
+#df = pd.read_pickle(feature_file)
 
 # =========================
 # 2. MODEL FEATURES
@@ -24,8 +24,12 @@ current_row = df.iloc[[-1]].copy()
 # =========================
 # 3. LOAD MODEL
 # =========================
-model = joblib.load("XGBoost_karachi_aqi_model.pkl")
-print("✓ Model loaded")
+#model = joblib.load("XGBoost_karachi_aqi_model.pkl")
+#print("✓ Model loaded")
+BASE_DIR = Path(__file__).resolve().parent
+
+feature_file = BASE_DIR / "feature_engineered_historical_with_realtime.pkl"
+model = joblib.load(BASE_DIR / "XGBoost_karachi_aqi_model.pkl")
 
 # =========================
 # 4. 3-DAY RECURSIVE PREDICTION
@@ -42,10 +46,18 @@ for day in range(1, 4):
     current_row['pm2.5'] = current_row['pm2.5']
     current_row['pm2_5_rolling_7'] = current_row['pm2_5_rolling_7']
 
+    from datetime import timedelta
+    current_date = pd.to_datetime(
+    df[['year','month','day']].iloc[-1]
+)
 
-    current_row['day'] += 1
-    current_row['dayofweek'] = (current_row['dayofweek'] + 1) % 7
-    current_row['is_weekend'] = current_row['dayofweek'].isin([5, 6]).astype(int)
+    current_date += timedelta(days=1)
+    current_row['year'] = current_date.year
+    current_row['month'] = current_date.month
+    current_row['day'] = current_date.day
+    current_row['dayofweek'] = current_date.dayofweek
+    current_row['is_weekend'] = int(current_date.dayofweek >= 5)
+
 
 # =========================
 # 5. OUTPUT
@@ -91,7 +103,9 @@ plt.xlabel("Forecast Day")
 plt.ylabel("AQI")
 plt.title("3-Day AQI Forecast (Karachi)")
 plt.grid(True)
-plt.show()
+plt.savefig("aqi_3day_forecast.png")
+plt.close()
+
 
 # =========================
 # 8. Save predictions to CSV
